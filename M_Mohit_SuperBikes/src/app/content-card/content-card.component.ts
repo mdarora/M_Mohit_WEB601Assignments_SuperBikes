bdce8c6d55be260affbbd53e92776afcfff530dd
mdarora <mdarora0001@gmail.com>
2023-02-08 14:31:20 -0500

    removed references of content-list class

--- M_Mohit_SuperBikes/src/app/content-card/content-card.component.ts
+++ M_Mohit_SuperBikes/src/app/content-card/content-card.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
@@ -8,31 +7,4 @@ import { ContentList } from '../helper-files/content-list';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent {
-  contentList: ContentList;
-
-  constructor(){
-    this.contentList = new ContentList();
-
-    this.contentList.add({id: 0,
-      title: "Kawasaki Ninja ZX10R",
-      description: "The Kawasaki Ninja ZX-10R is a motorcycle in the Ninja sport bike series from the Japanese manufacturer Kawasaki, the successor to the Ninja ZX-9R. It was originally released in 2004 and has been updated and revised throughout the years. It combines an ultra-narrow chassis, low weight, and radial brakes.",
-      creator: "Kawasaki Motorcycle & Engine Company",
-      imgURL: "https://content2.kawasaki.com/ContentStorage/CKM/Products/5016/ba433ac4-0669-47e4-a7f4-735c91bb0362.jpg?w=510&h=340&mode=crop",
-      type: "Super Sport"
-    });
-    this.contentList.add({id: 1,
-      title: "Yamaha YZF R1",
-      description: "The Yamaha YZF-R1, or simply R1, is a 1,000 cc-class sports motorcycle made by Yamaha. It was first released in 1998, undergoing significant updates in 2000, 2002, 2004, 2006, 2007, 2009, 2015, 2018 and 2020.",
-      creator: "Yamaha Motor Company",
-      imgURL: "https://dd5394a0b8ca8e97ba29-abf76f3d91a2125517d6c7c409f095c7.ssl.cf1.rackcdn.com/content/common/Models/2023/4dc57ed5-4a65-4963-a2d6-afd571510f59.png",
-      type: "Super Sport"
-    });
-    this.contentList.add({id: 2,
-      title: "Kawasaki Ninja H2",
-      description: "The Kawasaki Ninja H2 is a supercharged supersport-class motorcycle in the Ninja sports bike series manufactured by Kawasaki, featuring a variable-speed centrifugal supercharger.",
-      creator: "Kawasaki Motorcycle & Engine Company",
-      imgURL: "https://content2.kawasaki.com/ContentStorage/CKM/ProductTrimGroup/31/cbcb96d1-4e57-4563-9ccf-0b3ce735ca38.jpg?w=750",
-      type: "supercharged supersport"
-    });
-  }
 }
