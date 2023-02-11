923c43119759e9f958fcd87e8c789b5df7e46b9b
mdarora <mdarora0001@gmail.com>
2023-02-11 14:34:33 -0500

    filtering contents arrray

--- M_Mohit_SuperBikes/src/app/pipes/content-type.pipe.ts
+++ M_Mohit_SuperBikes/src/app/pipes/content-type.pipe.ts
@@ -1,12 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Pipe({
   name: 'contentType'
 })
 export class ContentTypePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(contents: Content[], type?: string): Content[] {
+    if(!contents){
+      return [];
+    }
+
+    if (!type){
+      return contents.filter(content => !content.type);
+    }
+
+    return contents.filter(content => content.type === type);
   }
 
 }
