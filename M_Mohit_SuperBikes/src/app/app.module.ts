5092cfebbf9b4af9cf7643df10beb34d76e5af47
mdarora <mdarora0001@gmail.com>
2023-02-18 01:35:42 -0500

    created new directive

--- M_Mohit_SuperBikes/src/app/app.module.ts
+++ M_Mohit_SuperBikes/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentTypePipe } from './pipes/content-type.pipe';
+import { HoverAffectDirective } from './directives/hover-affect.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
     ContentListComponent,
-    ContentTypePipe
+    ContentTypePipe,
+    HoverAffectDirective
   ],
   imports: [
     BrowserModule,
