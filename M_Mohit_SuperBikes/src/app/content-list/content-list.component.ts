b9eb71d2a0139862290c8935160428c02973f80f
mdarora <mdarora0001@gmail.com>
2023-02-18 01:31:48 -0500

    Added default image for bike

--- M_Mohit_SuperBikes/src/app/content-list/content-list.component.ts
+++ M_Mohit_SuperBikes/src/app/content-list/content-list.component.ts
@@ -10,6 +10,7 @@ export class ContentListComponent {
   contents: Content[];
   searchTitle: string = "";
   titleFound: boolean | null = null;
+  defaultBike: string = '/assets/images/drawBike.jpg'
 
 
   constructor(){
@@ -29,7 +30,8 @@ export class ContentListComponent {
         description: "The Yamaha YZF-R1, or simply R1, is a 1,000 cc-class sports motorcycle made by Yamaha. It was first released in 1998, undergoing significant updates in 2000, 2002, 2004, 2006, 2007, 2009, 2015, 2018 and 2020.",
         creator: "Yamaha Motor Company",
         imgURL: "https://dd5394a0b8ca8e97ba29-abf76f3d91a2125517d6c7c409f095c7.ssl.cf1.rackcdn.com/content/common/Models/2023/4dc57ed5-4a65-4963-a2d6-afd571510f59.png",
-        type: "Super Sport"
+        type: "Super Sport",
+        tags: ["Yamaha", "R1", "Super Sport"]
       },
       {
         id: 2,
@@ -37,13 +39,13 @@ export class ContentListComponent {
         description: "The Kawasaki Ninja H2 is a supercharged supersport-class motorcycle in the Ninja sports bike series manufactured by Kawasaki, featuring a variable-speed centrifugal supercharger.",
         creator: "Kawasaki Motorcycle & Engine Company",
         imgURL: "https://content2.kawasaki.com/ContentStorage/CKM/ProductTrimGroup/31/cbcb96d1-4e57-4563-9ccf-0b3ce735ca38.jpg?w=750",
+        tags: ["Kawasaki", "NinjaH2", "Hyper Sport"]
       },
       {
         id: 3,
         title: "BMW S1000RR",
         description: "BMW S1000RR is a race oriented sport bike initially made by BMW Motorrad to compete in the 2009 Superbike World Championship, that is now in commercial production. It was introduced in Munich in April 2008, and is powered by a 999 cc four-cylinder engine redlined at 14,200 rpm.",
         creator: "BMW Motorrad",
-        imgURL: "https://images.unsplash.com/photo-1635073908681-b4dfbd6015e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
         type: "Super Sport"
       },
       {
@@ -66,8 +68,8 @@ export class ContentListComponent {
         title: "Yamaha MT-10",
         description: "The Yamaha MT-10 is a standard motorcycle made by Japanese motorcycle manufacturer Yamaha in their MT motorcycle series, first sold in 2016. It was introduced at the 2015 EICMA in Milan, Italy. It is the flagship member of the MT range from Yamaha.",
         creator: "Yamaha Motor Company",
-        imgURL: "https://cdn-tp3.mozu.com/27878-44719/cms/44719/files/0de49048-2615-4155-aba1-fe0f35108102",
-        type: "Naked Super Sport"
+        type: "Naked Super Sport",
+        tags: ["Yamaha", "MT-10", "Naked Sport"]
       },
       {
         id: 7,
