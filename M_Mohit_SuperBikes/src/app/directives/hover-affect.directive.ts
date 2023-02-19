ff8cbe49936df250fc57a8e144b9a16e4325e9b4
mdarora <mdarora0001@gmail.com>
2023-02-19 14:08:34 -0500

    applied same directive on cards for border change (bonus)

--- M_Mohit_SuperBikes/src/app/directives/hover-affect.directive.ts
+++ M_Mohit_SuperBikes/src/app/directives/hover-affect.directive.ts
@@ -13,6 +13,8 @@ export class HoverAffectDirective {
       this.elm.nativeElement.style.textDecoration = this.hoverAffect;
     } else if(this.hoverAffect === 'bold'){
       this.elm.nativeElement.style.fontWeight = this.hoverAffect;
+    } else if(this.hoverAffect === 'border'){
+      this.elm.nativeElement.classList.add('hoverBorder');
     }
   }
   @HostListener('mouseleave') onMouseLeave(){
@@ -20,6 +22,8 @@ export class HoverAffectDirective {
       this.elm.nativeElement.style.textDecoration = 'none';
     } else if(this.hoverAffect === 'bold'){
       this.elm.nativeElement.style.fontWeight = 'normal';
+    } else if(this.hoverAffect === 'border'){
+      this.elm.nativeElement.classList.remove('hoverBorder');
     }
   }
 
