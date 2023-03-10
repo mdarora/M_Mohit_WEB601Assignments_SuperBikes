1ee3d4011d352841d47f18e5847eb07a25d6ec18
mdarora <mdarora0001@gmail.com>
2023-03-10 02:38:13 -0500

    Added messages

--- M_Mohit_SuperBikes/src/app/services/super-bike.service.ts
+++ M_Mohit_SuperBikes/src/app/services/super-bike.service.ts
@@ -2,21 +2,24 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Content } from '../helper-files/content-interface';
 import { contents } from '../helper-files/ContentDb';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SuperBikeService {
 
-  constructor() { }
+  constructor(private MessageService: MessageService) { }
 
   getBikes(): Observable<Content[]>{
     const bikes = contents;
+    this.MessageService.add("Content array loaded!");
     return of(bikes);
   }
 
   getBikeById(id: number): Observable<any>{
     const bike = contents.find(content => content.id === id);
+    this.MessageService.add(`Content Item at id: ${id}`);
     return of(bike);
   }
 }
