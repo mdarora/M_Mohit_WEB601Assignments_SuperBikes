00b7964ebbbe78eacc6c8de6109b001369bec162
mdarora <mdarora0001@gmail.com>
2023-03-16 01:25:21 -0400

    implented servicxe to use inMemory web api

--- M_Mohit_SuperBikes/src/app/services/in-memory-data.service.ts
+++ M_Mohit_SuperBikes/src/app/services/in-memory-data.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Content } from '../helper-files/content-interface';
+import { contents } from '../helper-files/ContentDb';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
 
   constructor() { }
+
+  createDb(){
+    const bikes : Content[] = contents;
+    return {bikes};
+  }
+
 }
