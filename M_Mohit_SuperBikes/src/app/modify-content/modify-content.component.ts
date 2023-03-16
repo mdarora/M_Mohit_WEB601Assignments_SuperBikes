eec8033cafa7621e3d8c0b24987e8a0577a581ec
mdarora <mdarora0001@gmail.com>
2023-03-16 04:18:44 -0400

    added form to add new bike and emitting a new event

--- M_Mohit_SuperBikes/src/app/modify-content/modify-content.component.ts
+++ M_Mohit_SuperBikes/src/app/modify-content/modify-content.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-modify-content',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./modify-content.component.scss']
 })
 export class ModifyContentComponent {
+  newBike: any = {};
+  @Output() addBikeEvent = new EventEmitter<Content>();
 
+  addNewBike(){
+    this.addBikeEvent.emit(this.newBike);
+    this.newBike = {};
+  }
 }
