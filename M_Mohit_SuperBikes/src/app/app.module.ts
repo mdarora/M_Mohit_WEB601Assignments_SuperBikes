ee97d9338dfd46e5c6504693beacb10bd2e0e2ce
mdarora <mdarora0001@gmail.com>
2023-04-07 13:51:06 -0400

    Added angular material

--- M_Mohit_SuperBikes/src/app/app.module.ts
+++ M_Mohit_SuperBikes/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './services/in-memory-data.service';
 import { ModifyContentComponent } from './modify-content/modify-content.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
  
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { ModifyContentComponent } from './modify-content/modify-content.componen
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
       dataEncapsulation: false,
       delay: 1000
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
