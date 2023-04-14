aa0d42ef7ff0450e9e5da828096415b256b02061
mdarora <mdarora0001@gmail.com>
2023-04-14 13:51:33 -0400

    showing not found page on invalid url

--- M_Mohit_SuperBikes/src/app/app-routing.module.ts
+++ M_Mohit_SuperBikes/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentDetailComponent } from './content-detail/content-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -18,6 +19,10 @@ const routes: Routes = [
   {
     path: 'contents/:id',
     component: ContentDetailComponent
+  },
+  {
+    path: "**",
+    component: PageNotFoundComponent
   }
 ];
 
